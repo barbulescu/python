97853c5df1e99c600343bb851629e8890df31e12
Marius Barbulescu <marius.barbulescu@gmail.com>
2020-12-22 20:05:15 +0100

    static

---
+++ oops/static.py
@@ -0,0 +1,16 @@
+class Player:
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+        Player.count += 1
+
+    @staticmethod
+    def get_count():
+        return Player.count
+
+
+messi = Player("Messi")
+ronaldo = Player("Ronaldo")
+print(Player.count)
+print(Player.get_count())
