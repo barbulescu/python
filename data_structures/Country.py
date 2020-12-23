55d485882cd5fb5e04b99ee2b117d274c88ae326
Marius Barbulescu <marius.barbulescu@gmail.com>
2020-12-23 07:08:28 +0100

    country

---
+++ data_structures/Country.py
@@ -0,0 +1,28 @@
+from operator import attrgetter
+
+
+class Country:
+    def __init__(self, name, population, area):
+        self.population = population
+        self.name = name
+        self.area = area
+
+    def __repr__(self):
+        return repr((self.name, self.area, self.population))
+
+
+countries = [
+    Country('India', 1200, 100),
+    Country('China', 1400, 200),
+    Country('USA', 120, 300)
+]
+
+countries.append(Country('Russia', 80, 900))
+
+countries.sort(key=attrgetter('population'), reverse=True)
+print(countries)
+
+print(max(countries, key=attrgetter('population')))
+print(min(countries, key=attrgetter('population')))
+print(max(countries, key=attrgetter('area')))
+print(min(countries, key=attrgetter('area')))
